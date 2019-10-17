a4922ecf730d53067e1a431dbaf72e61dfa8ca6e
User <User@Workbook-Pro.local>
2019-10-17 17:23:42 +0300

    Frontend

--- run.sh
+++ run.sh
@@ -2,4 +2,22 @@
 rm -r deploy/bitcoin/regtest/blocks/*
 rm deploy/bitcoin/regtest/keys.d3.wallet
 rm deploy/bitcoin/regtest/transfers.d3.wallet
-docker-compose -f deploy/docker-compose.yml up -d
+
+# Start backend
+if docker-compose -f deploy/docker-compose.yml up -d; then
+  echo Backend containers have been started
+else
+  echo Cannot start backend
+  exit 1
+fi
+
+readonly var backoffice="back-office"
+
+# Start frontend
+if yarn --cwd $backoffice serve; then
+  echo Frontend has been started
+else
+  echo Cannot start frontend
+  exit 1
+fi
+
