e91e9e255d27530cefb2d8fafb5153f6dcc4a6c2
dolgopolovwork <37849469+dolgopolovwork@users.noreply.github.com>
2019-10-18 10:35:15 +0300

    Frontend (#2)

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ else
   exit 1
 fi
 
-# Submodules
+# Backend submodules
 declare -a repos=(
   "chain-adapter"
   "d3-btc"
@@ -18,13 +18,32 @@ declare -a repos=(
   "report-system"
   "business-rules"
 )
+readonly var backoffice="back-office"
 
+# Update frontend
+if git -C $backoffice checkout develop --force && git -C $backoffice pull origin develop --rebase; then
+  echo Frontend repo has been updated successfully
+else
+  echo Cannot update frontend repo
+  exit 1
+fi
+
+# Build frontend
+if yarn --cwd $backoffice; then
+  echo Frontend has been built successfully
+else
+  echo Cannot build frontend
+  exit 1
+fi
+
+# Build backend
 for repo in "${repos[@]}"; do
   # Checkout to develop, pull and build docker images
-  if git -C $repo checkout develop && git -C $repo pull origin develop --rebase && $repo/gradlew -p $repo clean dockerBuild; then
+  if git -C $repo checkout develop --force && git -C $repo pull origin develop --rebase && $repo/gradlew -p $repo clean dockerBuild; then
     echo All projects have been built successfully
   else
     echo Cannot build project $repo
     exit 1
   fi
 done
+
